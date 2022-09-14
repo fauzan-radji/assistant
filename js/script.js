15bcffc7cf3bb193437a9ffdeaa2019c6667fd88
Tri Putra Fauzan H Radji <triputrafauzan@gmail.com>
2022-09-15 01:12:44 +0800

    googling feature

--- js/script.js
+++ js/script.js
@@ -8,20 +8,26 @@ const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
 recognition.addEventListener("result", (e) => {
-  console.log({ result: e });
+  const transcript = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join(" ");
+
+  updateText(transcript);
+  if (e.results[0].isFinal) {
+    addText(transcript);
+
+    // if transcript starts with 'carikan' then it will googling
+    if (transcript.toLowerCase().startsWith("carikan")) {
+      const query = transcript.split("carikan ").join("");
+      open(`http://www.google.com/search?hl=en&q=${normalizeQuery(query)}`);
+    }
+  }
 });
 
-recognition.addEventListener("error", (e) => {
-  console.log({ error: e.error });
-});
+recognition.addEventListener("error", (e) => console.log({ error: e.error }));
 
-recognition.addEventListener("speechstart", (e) => {
-  console.log({ speechstart: e });
-});
-
-recognition.addEventListener("end", (e) => {
-  recognition.start();
-});
+recognition.addEventListener("end", (e) => recognition.start());
 
 recognition.start();
 
@@ -36,4 +42,12 @@ function addText(text) {
 
 function updateText(text) {
   input.value = text;
+}
+
+function normalizeQuery(q) {
+  return q
+    .replace(":", "%3A")
+    .replace("+", "%2B")
+    .replace("&", "%26")
+    .replace(" ", "+");
 }
